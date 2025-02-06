31b0029e80bf55e38163f7bb7be3fab91f57c1b5
- <->
2025-02-06 17:06:00 +0300

    first

---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+def load_config(filename):
+    config = {}
+    with open(filename, "r") as file:
+        for line in file:
+            key, value = line.strip().split("=", 1)
+            config[key] = value
+    return config
+
+config = load_config("codes.txt")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+redis = get_redis_connection(
+    host=config.get("host"),
+    port=config.get("port"),
+    password=config.get("password"),
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+    
+    class Meta:
+        database = redis
+        model_config = {"arbitrary_types_allowed": True}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/products")    
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
