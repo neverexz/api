005796fada39b7d9987fbe56c5ac39558ee92a32
neverexz <cool-eg@mail.ru>
2025-02-11 21:46:39 +0300

    smth

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
+my_posts = []
+ 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+async def get_posts():
+    return my_posts
+
+@app.post("/posts")
+async def create_posts(post: Post):
+    my_posts.append(post.model_dump())
+    return {"data": post}
