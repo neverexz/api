adb523901f8977ebbccea4806cb2b129c031702a
neverexz <cool-eg@mail.ru>
2025-02-12 22:37:40 +0300

    starter

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+import uvicorn
 
 app = FastAPI()
 
@@ -12,16 +14,40 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 my_posts = []
- 
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            print(post)
+            return post
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 async def get_posts():
+    print(my_posts)
     return my_posts
 
 @app.post("/posts")
 async def create_posts(post: Post):
-    my_posts.append(post.model_dump())
-    return {"data": post}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": my_posts}
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    print(id)
+    return {f"post_{id}": find_post(id)}
+    
+    
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host='127.0.0.1',
+        port=8000,
+        reload=True
+    )
