2e40b3703b29acca3c05655adb9a67259c473b0d
- <cool-eg@mail.ru>
2025-03-02 18:17:04 +0300

    test branch

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ while True:
     except Exception as error:
         print("Error: ", error)
         time.sleep(3)
-
+# метка
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
